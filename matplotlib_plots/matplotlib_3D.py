61520a9b25685fd28a1a9fc4a20bce50371f1f4e
hongkangzhu <hz2551@columbia.edu>
2022-04-29 12:38:06 -0400

    add comments to 3D plot

--- matplotlib_plots/matplotlib_3D.py
+++ matplotlib_plots/matplotlib_3D.py
@@ -1,13 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-fig = plt.figure(figsize=(5, 3))
-ax = plt.axes(projection='3d')
+fig = plt.figure(figsize=(5, 3))    # create a figure
+ax = fig.add_subplot(projection='3d')   # add an axes to the figure
 
+# create numerical 2D arrays to plot
 x = np.linspace(-1, 1, 100)  # make the x-array with 100 numbers
 y = np.linspace(-1, 1, 100)  # make the y-array with 100 numbers
 X, Y = np.meshgrid(x, y)  # make the 2D meshgrid of the X-Y plane
-
 Z = X ** 2 + Y ** 2  # make the 2D array 'Z' defined on the X-Y plane
 
 ax.plot_surface(X, Y, Z, cmap='coolwarm')
